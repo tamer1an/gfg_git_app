131008887e8232677781ea94654a86e9b8d60981
Andrii Trybynenko <tamerjlah@gmail.com>
2022-02-11 19:46:14 +0100

    #1 POC: organization request with apollo client

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,65 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {
+    ApolloClient,
+    InMemoryCache,
+    ApolloProvider,
+    useQuery,
+    gql,
+    createHttpLink,
+} from "@apollo/client";
+import { setContext } from '@apollo/client/link/context';
+const httpLink = createHttpLink({
+    uri: 'https://api.github.com/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+    // get the authentication token from local storage if it exists
+    const token = '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';
+    // return the headers to the context so httpLink can read them
+    return {
+        headers: {
+            ...headers,
+            authorization: token ? `Bearer ${token}` : "",
+        }
+    }
+});
+const githubClient = new ApolloClient({
+    uri: 'https://api.github.com/graphql',
+    cache: new InMemoryCache(),
+    link: authLink.concat(httpLink),
+});
+
+const GIT_ORG = gql`
+    query {
+        organization(login: "github-tools") {
+            name
+            url
+        }
+    }
+`;
+
+function Organization() {
+    const { loading, error, data } = useQuery(GIT_ORG);
+    console.log(data)
+    if (loading) return <p>Loading...</p>;
+    if (error) return <p>Error :(</p>;
+
+    return (
+        <div>
+            <p>
+                {data.organization.name}
+            </p>
+        </div>
+    );
+}
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+      <ApolloProvider client={githubClient}>
+        <Organization />
+      </ApolloProvider>,
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
